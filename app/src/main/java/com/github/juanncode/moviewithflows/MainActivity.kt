d0fb108060aca424a421e65df068a727188d0c86
Juan Carlos Ortiz H <juanortizmg@gmail.com>
2022-02-25 16:07:07 -0500

    Add basic design for buttons and editText

---
+++ app/src/main/java/com/github/juanncode/moviewithflows/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.github.juanncode.moviewithflows
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        supportActionBar?.hide()
+
+    }
+}
